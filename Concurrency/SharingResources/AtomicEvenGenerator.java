6bd232720877bc537df97582626232fe8f2cef7a
ZhanpengDiao@Mac <diaozhanpeng1015@gmail.com>
2018-03-21 23:53:39 +1100

    atomic class

---
+++ Concurrency/SharingResources/AtomicEvenGenerator.java
@@ -0,0 +1,21 @@
+// rewrite MutexEvenGenerator with atomic class
+// all forms of synchronization has been eliminated
+// but it is safer to use lock/sync
+
+package SharingResources;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class AtomicEvenGenerator extends IntGenerator{
+
+    private AtomicInteger currentValue = new AtomicInteger(0);
+
+    @Override
+    public int next() {
+        return currentValue.addAndGet(2);
+    }
+
+    public static void main(String[] args) {
+        EvenChecker.test(new AtomicEvenGenerator());
+    }
+}
